efbcb904270902b56d6e524146dcdcefb1e9b7c7
Kevin Zheng <kzhen006@fiu.edu>
2025-04-16 03:38:58 -0400

    homescreen now has app bar of white svgs to make it look nicer. Also reminder that attirubtes updated on appwrite.

--- lib/features/home/view/home_view.dart
+++ lib/features/home/view/home_view.dart
@@ -1,26 +1,70 @@
-// import 'package:flutter/cupertino.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pixelverse_clone/constants/constants.dart';
 
-// import 'package:flutter_svg/svg.dart';
-// import 'package:pixelverse_clone/constants/constants.dart';
+import 'package:flutter_svg/svg.dart';
 // import 'package:pixelverse_clone/features/home/widgets/side_drawer.dart';
 // // import 'package:pixelverse_clone/features/tweet/views/create_tweet_view.dart';
-// import 'package:pixelverse_clone/theme/pallete.dart';
+import 'package:pixelverse_clone/theme/pallete.dart';
 
-class HomeView extends ConsumerWidget {
+class HomeView extends StatefulWidget {
   static route() => MaterialPageRoute(
         builder: (context) => const HomeView(),
       );
   const HomeView({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold();
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+final appBar = UIConstants.appBar();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar,
+      bottomNavigationBar: CupertinoTabBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              AssetsConstants.homeFilledIcon,
+              color: Pallete.whiteColor,
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              AssetsConstants.searchIcon,
+              color: Pallete.whiteColor,
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              AssetsConstants.notifFilledIcon,
+              color: Pallete.whiteColor,
+            ),
+          ),
+        ]
+        ),
+    );
   }
+}
+// class HomeView extends ConsumerWidget {
+//   static route() => MaterialPageRoute(
+//         builder: (context) => const HomeView(),
+//       );
+//   const HomeView({super.key});
+
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     return Scaffold(
+//       appBar: ,
+//     );
+//   }
   // @override
   // State<HomeView> createState() => _HomeViewState();
-}
+// }
 
 // class _HomeViewState extends State<HomeView> {
 //   int _page = 0;
